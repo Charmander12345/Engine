3310344cdc7c15119c16aab37185668bf73d779b
conno <conno@GNTHER>
2025-12-30 13:52:34 +0100

    Projektdateien hinzufügen.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+﻿// Engine.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void runPlaceholderStartup()
+{
+    cout << "Engine Project starting up..." << endl;
+    const auto delay = chrono::milliseconds(750);
+    cout << "Initializing logging system..." << endl;
+    this_thread::sleep_for(delay);
+    cout << "Loading configuration files..." << endl;
+    this_thread::sleep_for(delay);
+    cout << "Connecting to services..." << endl;
+    this_thread::sleep_for(delay);
+    cout << "Finalizing startup..." << endl;
+    this_thread::sleep_for(delay);
+    cout << "Startup complete. Ready to accept commands." << endl;
+}
+
+int main()
+{
+    runPlaceholderStartup();
+    return 0;
+}
